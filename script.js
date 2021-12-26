5481af92bafce7669d6732db0031d3bc8ff161de
Blib <olivia.joy.hanson@gmail.com>
2021-12-26 14:10:28 -0500

    created file, added randomNumber and words array

---
+++ script.js
@@ -0,0 +1,13 @@
+const randomNumber = (num) => {
+    return Math.random(Math.floor() * num)
+};
+
+const words = {
+    nouns: ["car", "dog", "cat", "human", "cup", "fish"],
+    adverbs: ["freakishly", "stupidly", "voraciously", "gloriously", "shyly"],
+    verbs: ["ran", "spun", "punched", "climbed", "ate"]
+};
+
+const phrases = [
+    
+]
