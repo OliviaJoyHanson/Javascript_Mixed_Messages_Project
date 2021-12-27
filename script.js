c47e62379b047341473a1ff973cd8cc14660fe12
Blib <olivia.joy.hanson@gmail.com>
2021-12-27 01:10:15 -0500

    added adLibPhrase method

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const randomNumber = () => {
     const num = nums[Math.floor(Math.random() * nums.length)];
     return num
 };
-console.log(randomNumber(5));
 
 const words = {
     nouns: ["car", "dog", "cat", "human", "cup", "fish"],
@@ -17,8 +16,17 @@ const phrases = [
     "What the noun?! They adverb verb into the barn!"
 ];
 
-const adLibPhrase = (num) => {
-    const ranNum = randomNumber(num);
+const adLibPhrase = () => {
+    const ranNum = randomNumber();
+    
+    const noun = words[nouns[randomNumber()]];
+    const adverb = words[abverbs[randomNumber()]];
+    const verb = words[verbs[randomNumber()]];
 
+    const phrase = phrases[ranNum];
 
+    const splitPhrase = phrase.split(" ");
+    console.log(splitPhrase);
 }
+
+adLibPhrase();
