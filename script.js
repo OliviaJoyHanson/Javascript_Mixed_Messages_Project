08dbfad4b23740f6fbe913163be18154f9e4e780
Blib <olivia.joy.hanson@gmail.com>
2021-12-27 00:51:43 -0500

    added phrases array

--- script.js
+++ script.js
@@ -9,5 +9,8 @@ const words = {
 };
 
 const phrases = [
-    
-]
+    "The noun verb, adverb, over the moon.",
+    "Did you see how he verb, adverb, into the noun?",
+    "What the noun?! They adverb verb into the barn!"
+];
+
