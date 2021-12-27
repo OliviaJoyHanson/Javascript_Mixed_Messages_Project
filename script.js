9099ca561664001a01de2615f479d22c43beb78b
Blib <olivia.joy.hanson@gmail.com>
2021-12-27 01:00:55 -0500

    made randomNumber method work without argument

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const randomNumber = (num) => {
-    return Math.random(Math.floor() * num)
+const randomNumber = () => {
+    const nums = [1,2,3,4,5]
+    const num = nums[Math.floor(Math.random() * nums.length)];
+    return num
 };
+console.log(randomNumber(5));
 
 const words = {
     nouns: ["car", "dog", "cat", "human", "cup", "fish"],
@@ -14,3 +17,8 @@ const phrases = [
     "What the noun?! They adverb verb into the barn!"
 ];
 
+const adLibPhrase = (num) => {
+    const ranNum = randomNumber(num);
+
+
+}
