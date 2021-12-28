1270f20fe2ea3345232a5405cf2727250b39f5da
Blib <olivia.joy.hanson@gmail.com>
2021-12-27 21:35:37 -0500

    added 2 more phrases and added splitPhrase.map()

--- script.js
+++ script.js
@@ -13,17 +13,41 @@ const words = {
 const phrases = [
     "The noun verb, adverb, over the moon.",
     "Did you see how he verb, adverb, into the noun?",
-    "What the noun?! They adverb verb into the barn!"
+    "What the noun?! They adverb verb into the barn!",
+    "I just watched as the noun adverb verb that thing.",
+    "He said he was at the market when, very adverb, a noun verb a vendor."
 ];
 
 const adLibPhrase = () => {
-    const ranNum = randomNumber();
+    // const ranNum = randomNumber();
     
     const noun = words.nouns[randomNumber()];
     const adverb = words.adverbs[randomNumber()];
     const verb = words.verbs[randomNumber()];
 
-    const phrase = phrases[ranNum];
+    var phrase = phrases[randomNumber()];
+    var splitPhrase = phrase.split(",");
 
-    const splitPhrase = phrase.split(" ");
+    // if splitPhrase.includes(w => "noun")
+    // if (splitPhrase.map(w => w === "noun")) {
+    //     splitPhrase[splitPhrase.indexOf("noun")] = noun;
+    // } else if (splitPhrase.map(w => w === "adverb")) {
+    //     splitPhrase[splitPhrase.indexOf("adverb")] = adverb;
+    // } else if (splitPhrase.map(w => w === "verb")) {
+    //     splitPhrase[splitPhrase.indexOf("verb")] = verb;
+    // }
+
+    splitPhrase.map(w => {
+        if (w === "noun") {
+            splitPhrase[splitPhrase.indexOf("noun")] = noun;
+        } else if (w === "adverb") {
+            splitPhrase[splitPhrase.indexOf("adverb")] = adverb;
+        } else if (w === "verb"){
+            splitPhrase[splitPhrase.indexOf("verb")] = verb;
+        }
+    })
+
+    console.log(splitPhrase)
 }
+
+adLibPhrase();
